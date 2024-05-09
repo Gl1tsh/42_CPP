db2e7289a88873b9b6da79c5f4a91d3f655cc5ca
Powlar <powlarofficial@gmail.com>
2024-05-09 16:51:38 +0200

    implement PhoneBook

--- 00/ex01/include/PhoneBook.hpp
+++ 00/ex01/include/PhoneBook.hpp
@@ -7,10 +7,13 @@ class PhoneBook
 {
 	public:
 		PhoneBook();
+		void add_contact(Contact the_contact);
+		Contact get_one_contact(int index);
+		int get_number_of_contact();
 
 	private:
-		Contact contacts[8];
+		Contact* contacts[8];
+		int position;
 };
 
 #endif
-
--- 00/ex01/src/PhoneBook.cpp
+++ 00/ex01/src/PhoneBook.cpp
@@ -1,1 +1,22 @@
 #include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook()
+{
+	position = 0;
+}
+
+void PhoneBook::add_contact(Contact the_contact)
+{
+	contacts[position] = &the_contact;
+	position++;
+}
+
+Contact PhoneBook::get_one_contact(int index)
+{
+	return *contacts[index];
+}
+
+int PhoneBook::get_number_of_contact()
+{
+	return position;
+}
