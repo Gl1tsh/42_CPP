805130aa53c4a98433e6e8de1ebad0428b21f504
Powlar <powlarofficial@gmail.com>
2024-05-09 14:38:39 +0200

    add ask function

--- 00/ex01/src/main.cpp
+++ 00/ex01/src/main.cpp
@@ -2,7 +2,19 @@
 #include "Contact.hpp"
 #include <iostream>
 
+std::string ask(std::string question)
+{
+	std::string answer;
+	std::cout << question << "?: ";
+	std::cin >> answer;
+	return answer;
+}
+
 int main(int argc, char **argv)
 {
+	std::string firstname;
+
+	firstname = ask("What is your firstname");
+
 	return 0;
 }
