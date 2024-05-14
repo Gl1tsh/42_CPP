0363167e97eb79773e9003778731b2a97468f62f
Powlar <powlarofficial@gmail.com>
2024-05-14 18:28:26 +0200

    add constructor and diplayAccountInfo

--- 00/ex02/src/Account.cpp
+++ 00/ex02/src/Account.cpp
@@ -1,15 +1,40 @@
 #include "Account.hpp"
 
+#include <cstdio>
+#include <ctime>
 #include <string>
 #include <iostream>
 
-// Constructor : default
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+
+// Constructor default
 Account::Account()
 {
-
+	_accountIndex = 0;
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
 }
 
-// Constructor : overload
+// Constructor
+Account::Account(int initial_deposit)
+{
+	_accountIndex = 0;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+
+	Account::_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";"
+			  << "amount:" << _amount << ";"
+			  << "created" << std::endl;
+}
+
+// Destructor
 Account::~Account()
 {
 
@@ -17,33 +42,33 @@ Account::~Account()
 
 int Account::getNbAccounts(void)
 {
-
+	return 1;
 }
 
 int Account::getTotalAmount(void)
 {
-
+	return 1;
 }
 
 int Account::getNbDeposits(void)
 {
-
+	return 1;
 }
 
 int Account::getNbWithdrawals(void)
 {
-
+	return 1;
 }
 
 void Account::displayAccountsInfos(void)
 {
-
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";"
+			  << "total:" << _totalAmount << ";"
+			  << "deposits:" << _totalNbDeposits << ";"
+			  << "withdrawals:" << _totalNbWithdrawals << ";"
+			  << std::endl;
 }
-
-// ================= constructor zone
-
-
-// =================
 
 void Account::makeDeposit(int deposit)
 {
@@ -52,12 +77,12 @@ void Account::makeDeposit(int deposit)
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-
+	return true;
 }
 
 int Account::checkAmount(void) const
 {
-
+	return 1;
 }
 
 void Account::displayStatus(void) const
@@ -65,11 +90,13 @@ void Account::displayStatus(void) const
 
 }
 
-// ===== private zone
-
 void Account::_displayTimestamp(void)
 {
+	std::time_t t = std::time(NULL);
+	std::tm* tm = std::localtime(&t);
 
+	char buffer[19];
+	std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", tm);
+
+	std::cout << buffer;
 }
-
-// ===================
