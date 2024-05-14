0a207006372e72c732d2b98900c33fde972ab64e
Powlar <powlarofficial@gmail.com>
2024-05-14 17:49:36 +0200

    initial ex02 with empty members functions

---
+++ 00/ex02/src/Account.cpp
@@ -0,0 +1,75 @@
+#include "Account.hpp"
+
+#include <string>
+#include <iostream>
+
+// Constructor : default
+Account::Account()
+{
+
+}
+
+// Constructor : overload
+Account::~Account()
+{
+
+}
+
+int Account::getNbAccounts(void)
+{
+
+}
+
+int Account::getTotalAmount(void)
+{
+
+}
+
+int Account::getNbDeposits(void)
+{
+
+}
+
+int Account::getNbWithdrawals(void)
+{
+
+}
+
+void Account::displayAccountsInfos(void)
+{
+
+}
+
+// ================= constructor zone
+
+
+// =================
+
+void Account::makeDeposit(int deposit)
+{
+
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int Account::checkAmount(void) const
+{
+
+}
+
+void Account::displayStatus(void) const
+{
+
+}
+
+// ===== private zone
+
+void Account::_displayTimestamp(void)
+{
+
+}
+
+// ===================
