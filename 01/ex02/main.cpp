e95974e2411f75a73a3d06764d12e9daefc74f2b
Powlar <powlarofficial@gmail.com>
2024-05-17 19:29:09 +0200

    01/ex02 initialise and add print for each mandatory

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <iostream>
+
+int main()
+{
+	// basic
+	std::string hi_this_is_brain = "HI THIS IS BRAIN";
+	//stringPTR
+	std::string* stringPTR = &hi_this_is_brain;
+	std::string& stringREF = hi_this_is_brain;
+	
+
+
+
+
+	std::cout << std::endl;
+	std::cout << "The memory address of the string variable     : " << &hi_this_is_brain << std::endl;
+	std::cout << "The memory address of the string variable     : " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF          : " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "The value of the string variable              : " << hi_this_is_brain << std::endl;
+	std::cout << "The value pointed to by stringPTR             : " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF             : " << stringREF << std::endl;
+	
+	return 0;
+}
