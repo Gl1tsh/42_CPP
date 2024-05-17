8e8494e0c3eda1438ae5db9220ac62a270697c98
Powlar <powlarofficial@gmail.com>
2024-05-17 19:31:18 +0200

    01/ex02 clean main

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -3,24 +3,19 @@
 
 int main()
 {
-	// basic
 	std::string hi_this_is_brain = "HI THIS IS BRAIN";
-	//stringPTR
 	std::string* stringPTR = &hi_this_is_brain;
 	std::string& stringREF = hi_this_is_brain;
-	
-
-
-
 
 	std::cout << std::endl;
 	std::cout << "The memory address of the string variable     : " << &hi_this_is_brain << std::endl;
 	std::cout << "The memory address of the string variable     : " << stringPTR << std::endl;
     std::cout << "The memory address held by stringREF          : " << &stringREF << std::endl;
+
 	std::cout << std::endl;
 	std::cout << "The value of the string variable              : " << hi_this_is_brain << std::endl;
 	std::cout << "The value pointed to by stringPTR             : " << *stringPTR << std::endl;
 	std::cout << "The value pointed to by stringREF             : " << stringREF << std::endl;
-	
+
 	return 0;
 }
