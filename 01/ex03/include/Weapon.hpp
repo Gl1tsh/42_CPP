d85b47f78df1ba1e0d3123a37cb75e6208091a80
Powlar <powlarofficial@gmail.com>
2024-05-18 13:52:02 +0200

    01/ex03 add attributes and methods to Weapon.hpp

--- 01/ex03/include/Weapon.hpp
+++ 01/ex03/include/Weapon.hpp
@@ -1,14 +1,17 @@
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
 
+#include <string>
+
 class Weapon
 {
 
 public:
+	const std::string& getType(std::string type);
+	std::string setType(std::string type);
 
 private:
-
-
-}
+	std::string type;
+};
 
 #endif
