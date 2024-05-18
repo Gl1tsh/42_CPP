7be669b6b68712b8105ea8bb6409f22b1055b72f
Powlar <powlarofficial@gmail.com>
2024-05-18 13:55:56 +0200

    01/ex03 add attributes and methods for HumanA and HumanB

--- 01/ex03/include/HumanA.hpp
+++ 01/ex03/include/HumanA.hpp
@@ -1,14 +1,18 @@
 #ifndef HUMANA_HPP
 #define HUMANA_HPP
 
+#include <string>
+
 class HumanA
 {
 
 public:
+	void attack(std::string weapon_type);
 
 private:
+	std::string Weapon;
+	std::string name;
 
-
-}
+};
 
 #endif
--- 01/ex03/include/HumanB.hpp
+++ 01/ex03/include/HumanB.hpp
@@ -1,14 +1,18 @@
 #ifndef HUMANB_HPP
 #define HUMANB_HPP
 
+#include <string>
+
 class HumanB
 {
 
 public:
+	void attack(std::string weapon_type);
 
 private:
+	std::string Weapon;
+	std::string name;
 
-
-}
+};
 
 #endif
