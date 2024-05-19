0ac153f7b121cae43a13818b7e1130ca258c7831
Powlar <powlarofficial@gmail.com>
2024-05-19 20:57:30 +0200

    01/ex04 clean main and finish

--- 01/ex04/src/main.cpp
+++ 01/ex04/src/main.cpp
@@ -1,25 +1,6 @@
 #include <string>
 #include <iostream>
-
-
-/*
- * chose a faire :
- *
- * ouvrir le fichier
- * lire une ligne apres l'autre
- * remplacer le mot trouver (s2) par un autre (s1)
- * fermer le fichier une fois fini
- *
- *
- * fonction pratique :
- * insert
- * find
- * substr
- * erase
- *
- *
-*/
-
+#include <fstream>
 
 std::string replace_me(std::string line, std::string search, std::string replace)
 {
@@ -35,44 +16,29 @@ std::string replace_me(std::string line, std::string search, std::string replace
 	return line;
 }
 
-
-
 int main(int argc, char **argv)
 {
 	if (argc != 4)
+	{
+		std::cout << "Wrong argument count" << std::endl;
 		return 1;
+	}
 	else
 	{
-		std::string line = argv[1];
+		std::string filename = argv[1];
 		std::string search = argv[2];
 		std::string replace = argv[3];
 
-		line = replace_me(line, search, replace);
-		std::cout << line << std::endl;
+		std::fstream file(filename, std::ios::in);
+		if (!file.is_open())
+		{
+			std::cout << "Cannot open " << filename << std::endl;
+			return 1;
+		}
+
+		std::string line;
+		while (std::getline(file, line))
+			std::cout << replace_me(line, search, replace) << std::endl;
 	}
 	return 0;
 }
-
-
-/*
-int main(int argc, char **argv)
-{
-	(void)argc;
-	std::string line = argv[1];
-	std::string search = argv[2];
-	std::string replace = argv[3];
-
-	std::string::size_type pos;
-
-	pos = line.find(search);
-	while (pos != std::string::npos)
-	{
-		line.erase(pos, search.size());
-		line.insert(pos, replace);
-		pos = line.find(search, pos + replace.size());
-	}
-	std::cout << line << std::endl;
-	return 0;
-}
-
-*/
