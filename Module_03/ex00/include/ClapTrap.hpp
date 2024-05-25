47d1cf38bed1febcee7f4cbf095664ff3cd5ffaf
Powlar <powlarofficial@gmail.com>
2024-05-25 16:36:15 +0200

    03/ex00 setup base for project

---
+++ Module_03/ex00/include/ClapTrap.hpp
@@ -0,0 +1,22 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+public:
+	ClapTrap(std::string name);
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+private:
+	std::string name;
+	unsigned int hitPoint;
+	unsigned int energyPoint;
+	unsigned int attackDamage;
+
+};
+
+#endif
---
+++ Module_03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,22 @@
+#include "ClapTrap.hpp"
+
+
+ClapTrap::ClapTrap(std::string name)
+{
+	this->name = name;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+
+}
