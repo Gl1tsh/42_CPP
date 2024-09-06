a7dd2d3ac9f771bc05b3556334b3ac043f4503c3
Powlar <powlarofficial@gmail.com>
2024-09-06 13:54:40 +0200

    07/ex02 change comment for better explaination

--- Module_07/ex01/src/main.cpp
+++ Module_07/ex01/src/main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+	// initialisation de 3 tableaux : int, char et string
 	int array_int[] = {1, 2, 3, 4, 5};
 	char array_char[] = {'a', 'b', 'c', 'd', 'e'};
 	std::string array_string[] = {"un,", " deux,", " trois,", " quatre,", " cinque"};
@@ -10,12 +11,11 @@ int main()
 	/*
 	utilisation du template iter pour afficher les éléments des tableaux ainsi que print
 
-	iter ressemble est setup comme cela :
+	iter est setup comme cela :
 	void iter(Type *array, int lenght, void (*function)(Type const &))
-	donc ici on a un tableau de int, un tableau de char et un tableau de string
 
 	pour etre plus opti, il faudrait egalement une fonction pour trouver la longeur total de chaque tableau
-	pour eviter d'ecrire 5 a chaque fois par exemple.
+	pour eviter d'ecrire 5 a chaque fois par exemple. Mais pas autorisé dans le sujet
 	*/
 
 	iter(array_int, 5, print);
