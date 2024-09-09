886db48a39a66e78512ee4206032986c06abbf56
Powlar <powlarofficial@gmail.com>
2024-09-09 15:41:05 +0200

    06/ex00 fix for all and special, adapted main for test

--- Module_06/ex00/src/ScalarConverter.cpp
+++ Module_06/ex00/src/ScalarConverter.cpp
@@ -28,54 +28,41 @@ ScalarConverter& ScalarConverter::operator=(ScalarConverter& source)
 
 void ScalarConverter::convert(const std::string& input)
 {
-	if (input == "+inf" || input == "-inf" || input == "nan")
-	{
-		std::cout << "char: impossible" << std::endl;
-		std::cout << "float: " << input << "f" << std::endl;
-		std::cout << "double: " << input << std::endl;
-		return;
-	}
-
-	if (input == "-inff" || input == "+inff" || input == "nanf")
-	{
-		std::cout << "char: impossible" << std::endl;
-		std::cout << "float: " << input.substr(0, input.length() - 1) << "f" << std::endl;
-		std::cout << "double: " << input.substr(0, input.length() - 1) << std::endl;
-		return;
-	}
-
 	// ============= Check si c'est un char et character imprimable
-	if (input.length() == 1 && isprint(input[0]) && !isdigit(input[0]))
+	if (input.length() == 1 && isprint(input[0]))
 		std::cout << "char: " << input[0] << std::endl;
 	else
-		std::cout << "char: error: not a character" << std::endl;
+		std::cout << "char: error: impossible" << std::endl;
 
 
 	// ============= Check si c'est un int (strol)
-	char *end;
 	errno = 0;
+	char *end;
 	long intValue = std::strtol(input.c_str(), &end, 10);
 
 	if (*end == '\0' && errno == 0)
 		std::cout << "int: " << intValue << std::endl;
 	else
-		std::cout << "int: error: not a int" << std::endl;
+		std::cout << "int: error: impossible " << std::endl;
 
 	// ============= Check si c'est un float
 	errno = 0;
-	float floatValue = std::strtof(input.c_str(), &end);
+	std::string inputCopy = input;
+	if (input == "+inff" || input == "-inff" || input == "nanf")
+		inputCopy.erase(inputCopy.end() - 1);
+	float floatValue = std::strtof(inputCopy.c_str(), &end);
 
-	if (*end == '\0' && errno == 0)
+	if (end != inputCopy.c_str() && *end == '\0' && errno == 0)
 		std::cout << "float: " << floatValue << "f" << std::endl;
 	else
-		std::cout << "float: error: not a float" << std::endl;
+		std::cout << "float: error: impossible" << std::endl;
 
-	// ============= Check si c'est un double 
+	// ============= Check si c'est un double
 	errno = 0;
 	double doubleValue = std::strtod(input.c_str(), &end);
 
-	if (*end == '\0' && errno == 0)
+	if (end != input.c_str() && *end == '\0' && errno == 0)
 		std::cout << "double: " << doubleValue << std::endl;
 	else
-		std::cout << "double: error: not a double" << std::endl;
+		std::cout << "double: error: impossible" << std::endl;
 }
--- Module_06/ex00/src/main.cpp
+++ Module_06/ex00/src/main.cpp
@@ -2,25 +2,58 @@
 #include <iostream>
 #include <string>
 
-/*
+
 int main()
 {
+	// Test case for empty string
+	std::cout << "\033[32m" << "TEST CASE FOR NOTHING STRING" << "\033[0m" << std::endl;
+	std::cout << "Test avec rien:" << std::endl;
+	ScalarConverter::convert("");
+	std::cout << std::endl;
+
+	// Test case for non-printable string
+	std::cout << "\033[32m" << "TEST CASE FOR NON-PRINTABLE STRING" << "\033[0m" << std::endl;
+	std::cout << "Test avec '\\n\\t':" << std::endl;
+	ScalarConverter::convert("\n\t");
+	std::cout << std::endl;
+
+	// Test case for special
+	std::cout << "\033[32m" << "TEST CASE FOR SPECIAL" << "\033[0m" <<  std::endl;
+	std::cout << "Test avec '+inf':" << std::endl;
+	ScalarConverter::convert("+inf");
+	std::cout << std::endl;
+
+	std::cout << "\nTest avec '-inf':" << std::endl;
+	ScalarConverter::convert("-inf");
+
+	std::cout << "\nTest avec 'nan':" << std::endl;
+	ScalarConverter::convert("nan");
+
+	std::cout << "\nTest avec '+inff':" << std::endl;
+	ScalarConverter::convert("+inff");
+
+	std::cout << "\nTest avec '-inff':" << std::endl;
+	ScalarConverter::convert("-inff");
+
+	std::cout << "\nTest avec 'nanf':" << std::endl;
+	ScalarConverter::convert("nanf");
+	std::cout << std::endl;
+
 	// Test cases for char
+	std::cout << "\033[32m" << "TEST CASE FOR CHAR" << "\033[0m" <<  std::endl;
 	std::cout << "Test avec 'a':" << std::endl;
 	ScalarConverter::convert("a");
 
 	std::cout << "\nTest avec '9':" << std::endl;
 	ScalarConverter::convert("9");
 
-	std::cout << "\nTest avec '@':" << std::endl;
-	ScalarConverter::convert("@");
-
-	// Test case for non-printable char
-	std::cout << "\nTest avec '\\n' (non-printable character):" << std::endl;
-	ScalarConverter::convert("\n");
+	std::cout << "\nTest avec 'coucou':" << std::endl;
+	ScalarConverter::convert("coucou");
+	std::cout << std::endl;
 
 	// Test cases for int
-	std::cout << "\nTest avec '123':" << std::endl;
+	std::cout << "\033[32m" << "TEST CASE FOR INT" << "\033[0m" <<  std::endl;
+	std::cout << "Test avec '123':" << std::endl;
 	ScalarConverter::convert("123");
 
 	std::cout << "\nTest avec '-123':" << std::endl;
@@ -31,9 +64,11 @@ int main()
 
 	std::cout << "\nTest avec '-999999999999999999999999999999' (int out of range):" << std::endl;
 	ScalarConverter::convert("-999999999999999999999999999999");
+	std::cout << std::endl;
 
 	// Test cases for float
-	std::cout << "\nTest avec '123.456':" << std::endl;
+	std::cout << "\033[32m" << "TEST CASE FOR FLOAT" << "\033[0m" << std::endl;
+	std::cout << "Test avec '123.456':" << std::endl;
 	ScalarConverter::convert("123.456");
 
 	std::cout << "\nTest avec '1e39' (float out of range):" << std::endl;
@@ -41,9 +76,11 @@ int main()
 
 	std::cout << "\nTest avec '-1e39' (float out of range):" << std::endl;
 	ScalarConverter::convert("-1e39");
+	std::cout << std::endl;
 
 	// Test cases for double
-	std::cout << "\nTest avec '1.7976931348623157e308' (double upper boundary):" << std::endl;
+	std::cout << "\033[32m" << "TEST CASE FOR DOUBLE" << "\033[0m" << std::endl;
+	std::cout << "Test avec '1.7976931348623157e308' (double upper boundary):" << std::endl;
 	ScalarConverter::convert("1.7976931348623157e308");
 
 	std::cout << "\nTest avec '-1.7976931348623157e308' (double lower boundary):" << std::endl;
@@ -57,8 +94,8 @@ int main()
 
 	return 0;
 }
-*/
 
+/*
 int main(int argc, char** argv)
 {
 	if (argc < 2) 
@@ -75,3 +112,4 @@ int main(int argc, char** argv)
 
 	return 0;
 }
+*/
